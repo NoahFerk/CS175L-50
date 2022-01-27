81307e315fb8db464a552ce2aa98146c73d777ce
NoahFerk <91210669+NoahFerk@users.noreply.github.com>
2022-01-26 19:12:51 -0500

    Stocks

---
+++ stocks(lab).py
@@ -0,0 +1,23 @@
+#CS175-50
+#Noah Ferker
+#stocks
+
+#Constants
+COMMISSION_RATE = 0.03
+NUM_SHARES = 2000
+PURCHASE_PRICE = 40.0
+SELLING_PRICE = 42.75
+#Variables
+amountPaidForStock = NUM_SHARES * PURCHASE_PRICE
+purchaseCommission = COMMISSION_RATE * amountPaidForStock
+totalPaid = amountPaidForStock + purchaseCommission
+stockSoldFor = NUM_SHARES * SELLING_PRICE
+sellingCommission = COMMISSION_RATE * stockSoldFor
+totalReceived = stockSoldFor - sellingCommission
+profitOrLoss = totalReceived - totalPaid
+#Display
+print("Amount paid for stock: ", amountPaidForStock , "$")
+print("Commission paid on the purchase: ", purchaseCommission,"$")
+print("Amount the stock sold for: ", stockSoldFor, "$")
+print("Commission paid on the sale: ", sellingCommission, "$")
+print("Profit (or loss if negative): ", profitOrLoss, "$")

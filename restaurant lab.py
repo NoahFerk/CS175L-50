4a9e1800f9554c7d317ec3017cbce175b48ac7b5
NoahFerk <91210669+NoahFerk@users.noreply.github.com>
2022-02-14 18:33:21 -0500

    Restaurant v2

--- restaurant lab.py
+++ restaurant lab.py
@@ -1,33 +1,36 @@
 #Noah Ferker
 #CS 175L 50
-#February 7 2022
+#February 14 2022
 
+keep_going = 'y'
 vegetarian = False
 vegan = False
 gluten_free = False
 
-is_vegetarian = input('Is anyone in your party vegetarian?')
-is_vegan = input('Is anyone in your party vegan?')
-is_gluten_free = input('Is anyone in your party gluten free?')
+while keep_going == 'y':
+    is_vegetarian = input('Is anyone in your party vegetarian?')
+    is_vegan = input('Is anyone in your party vegan?')
+    is_gluten_free = input('Is anyone in your party gluten free?')
 
-if is_vegetarian == 'yes':
-    vegetarian = True
-if is_vegan == 'yes':
-    vegan = True
-if is_gluten_free == 'yes':
-    gluten_free = True
+    if is_vegetarian == 'yes':
+        vegetarian = True
+    if is_vegan == 'yes':
+        vegan = True
+    if is_gluten_free == 'yes':
+        gluten_free = True
 
-print('Here are your resaurant choices:')
+    print('Here are your resaurant choices:')
 
-if vegetarian == False and vegan == False and gluten_free == False:
-    print('Joe\'s Gourmet Burgers')
+    if vegetarian == False and vegan == False and gluten_free == False:
+        print('Joe\'s Gourmet Burgers')
 
-if vegan == False and gluten_free == False:
-    print('Mama\'s Fine Italian')
+    if vegan == False and gluten_free == False:
+        print('Mama\'s Fine Italian')
 
-if vegan == False:
-    print('Main Street Pizza')
+    if vegan == False:
+        print('Main Street Pizza')
 
-print('Corner Cafe')
-print('Chef\'s Kitchen')
+    print('Corner Cafe')
+    print('Chef\'s Kitchen')
+    keep_going = input('Would you like to search again? ')
 

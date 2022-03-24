5e18f940fb9e1d1f8966a408447b28365c9e72f7
NoahFerk <91210669+NoahFerk@users.noreply.github.com>
2022-03-24 10:06:25 -0400

    v1

---
+++ AverageFromInput.py
@@ -0,0 +1,19 @@
+#Noah Ferker
+#CS 175L
+#Average From Input
+
+def main():
+    number_file = open('numbers.txt','r')
+    total = 0
+    counter = 0
+    for line in number_file:
+        number = float(line)
+        total += number
+        counter = counter + 1
+        print('I read in ', counter, 'number(s) Current number is: ', f'{number:>8.2f}','Total is: ', f'{total:>8.2f}')
+    avg = total/counter
+    print('Average: ', avg)
+    number_file.close()
+
+if __name__ == '__main__':
+    main()
